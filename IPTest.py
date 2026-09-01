24c452ac5a4be6bece5cf69656faebf06b8de9db
Zachary Kirkeby <zakirkeby@gmail.com>
2026-09-01 14:10:32 -0400

    scaffolding

---
+++ IPTest.py
@@ -0,0 +1,24 @@
+#### Just Scaffolding
+
+####################################### Part 1
+# Import from json file
+# grab ip from json
+# os command -> do ping test
+# if non responsive, note, skip
+# save / parse data
+# use api from https://tools.keycdn.com/geo to get coords
+# repeat
+
+# get own ip
+# ping test
+# repeat above steps
+
+# scatter plot based on return data
+
+# explanation part
+
+######################################## Part 2
+
+# select random 5
+# do new ping test stuff 
+# graphs

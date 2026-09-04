add7f3019d3cc3457936071381e619b5f40eca06
rluo.dev <34289582+rluodev@users.noreply.github.com>
2026-09-04 14:03:29 -0400

    fix typo

--- script.py
+++ script.py
@@ -17,7 +17,7 @@ def ping(ip: str) -> tuple[float, float, float]: # returns (min, max, avg)
         if platform.system() == "Windows":
             cmd = ["ping", "-n", "10", ip]
         else:
-            cmd = ["ping", "-c", "1", ip]
+            cmd = ["ping", "-c", "10", ip]
         res = subprocess.check_output(cmd)
         stats_str_full = res.decode(errors="replace").splitlines()[-1:][0] # string of last line of ping command including "round-trip min/avg/max/stddev =" part
         if platform.system() == "Windows":
